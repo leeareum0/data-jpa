64fad313d8cfe169e1e2804e4a59dc69df71acb4
이아름 <areum6321@gmail.com>
2024-03-28 00:49:26 +0900

    공통 인터페이스 설정 - 구현 클래스 대신 생성(Team Interfece 추가)

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,34 +1,8 @@
 package study.datajpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.datajpa.Entity.Member;
 import study.datajpa.Entity.Team;
 
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Team  t", Long.class)
-                .getSingleResult();
-    }
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
