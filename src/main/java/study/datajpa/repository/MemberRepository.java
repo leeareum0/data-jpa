e04ad91423a51aff1c94b662b0227681df5a1aa7
이아름 <areum6321@gmail.com>
2024-03-29 17:54:31 +0900

    쿼리 메서드 기능 - @Query 사용, 단일 값 조회 및 DTO로 직접 조회

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.Entity.Member;
+import study.datajpa.dto.MemberDto;
 
 import java.util.List;
 
@@ -18,4 +19,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //메서드에 JPQL 쿼리 작성 - 복잡할 때 사용
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    //Dto 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
