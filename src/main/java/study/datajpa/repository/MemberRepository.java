61cb6d29bbe08a73638348e2a3404a81941f26c9
이아름 <areum6321@gmail.com>
2024-03-29 17:26:09 +0900

    쿼리 메서드 기능 - @Query 사용해서 쿼리 정의, 메서드에 JPQL 쿼리 작성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -9,8 +9,13 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    //쿼리가 간단할 때 사용
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     //@Query(name = "Member.findByUsername") //생략 가능
     List<Member> findByUsername(@Param("username") String username);
+
+    //메서드에 JPQL 쿼리 작성 - 복잡할 때 사용
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
