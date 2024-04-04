7efd51a09faad597613bcaff77e4a923843962ff
이아름 <areum6321@gmail.com>
2024-04-04 23:43:35 +0900

    확장 기능 - 사용자 정의 리포지토리 구현, 사용자 정의 인터페이스 추가

---
+++ src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
@@ -0,0 +1,10 @@
+package study.datajpa.repository;
+
+import study.datajpa.Entity.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    List<Member> findMemberCustom();
+}
