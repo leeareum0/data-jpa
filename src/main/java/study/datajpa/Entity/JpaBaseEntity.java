8a2a2e220e71f0afabf27778ccb68db3cb095162
이아름 <areum6321@gmail.com>
2024-04-05 16:55:08 +0900

    확장 기능 - Auditing, 순수 JPA 사용 등록일, 수정일 엔티티 추가

---
+++ src/main/java/study/datajpa/Entity/JpaBaseEntity.java
@@ -0,0 +1,30 @@
+package study.datajpa.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate; //등록일
+    private LocalDateTime updatedDate; //수정일
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
