188bb1ffe6a6502f467a241b0b34eb980ba04e7d
이아름 <areum6321@gmail.com>
2024-04-05 17:26:43 +0900

    확장 기능 - Auditing, 스프링 데이터 JPA 사용 Auditing 적용 엔티티 추가

---
+++ src/main/java/study/datajpa/Entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package study.datajpa.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity extends BaseTimeEntity {
+
+    //@Baen 등록 - auditorProvider 호출
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
