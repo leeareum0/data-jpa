7f784249a0b359421ecb0f28f0fc7b876c38434c
이아름 <areum6321@gmail.com>
2024-04-05 18:17:36 +0900

    확장 기능 - Web 확장, 페이징 및 정렬 기능 사용

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,7 +2,9 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +23,23 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // Web 확장 - 도메인 클래스 컨버터 사용
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
-    
+
+    // Web 확장 - 페이징과 정렬
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
